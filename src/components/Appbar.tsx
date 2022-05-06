0e7f8cc5a66020199129bc6c6649ad8d845f8337
natsumi <aka.723n@gmail.com>
2022-05-06 13:44:34 +0900

    chore:pagesを右寄せ

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -91,7 +91,7 @@ const ResponsiveAppBar = () => {
           >
             LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'flex-end' } }}>
             {pages.map((page) => (
               <Button
                 key={page}
