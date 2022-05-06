b1bd5071c4de25bc1305ac561ac2db237ba26d36
natsumi <aka.723n@gmail.com>
2022-05-06 13:35:02 +0900

    feat: add Appbar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ResponsiveAppBar from './components/Appbar';
 
 export const App=()=> {
   return (
     <div className="App">
-
+      <ResponsiveAppBar/>
     </div>
   );
 };
