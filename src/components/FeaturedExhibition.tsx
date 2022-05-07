1f7e0422b9d06c4153c26609a1e04ed1bf21b703
natsumi <aka.723n@gmail.com>
2022-05-07 18:30:30 +0900

    chore: add IconButton

--- src/components/FeaturedExhibition.tsx
+++ src/components/FeaturedExhibition.tsx
@@ -6,9 +6,13 @@ import {
   CardActions,
   CardContent,
   CardMedia,
-  Container,
+  Container, IconButton,
   Typography
 } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import CreateIcon from '@mui/icons-material/Create';
 
 export const FeaturedExhibition=()=> {
   return (
@@ -28,16 +32,23 @@ export const FeaturedExhibition=()=> {
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Lizard
+              展示名
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica
+              美術館名
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              開催期間
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+
+            </IconButton>
+            <RemoveRedEyeIcon/>
+            <Button size="small"><CreateIcon/>投稿</Button>
+            <Button size="small"><BookmarkBorderIcon/>保存</Button>
           </CardActions>
         </Card>
       </Container>
