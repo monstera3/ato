570acbac76688c8063334fa9e757f09f58e4f423
natsumi <aka.723n@gmail.com>
2022-05-08 13:31:08 +0900

    feat: add Login.tsx,About.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Routes,Route } from "react-router-dom"
 import { ExhibitionList } from './components/ExhibitionList';
 import { MuseumList } from './components/MuseumList';
 import { Home } from './components/Home';
+import { About } from './components/About';
+import { Login } from './components/Login';
 
 export const App=()=> {
   return (
@@ -14,6 +16,8 @@ export const App=()=> {
       <SearchBar/>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="about" element={<About />}/>
+        <Route path="login" element={<Login />}/>
         <Route path="museums" element={<MuseumList />}/>
         <Route path="exhibitions" element={<ExhibitionList />}/>
       </Routes>
