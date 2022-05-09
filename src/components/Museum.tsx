74095d876fcbb15398752928896d8e5ebe214ea1
natsumi <aka.723n@gmail.com>
2022-05-09 20:22:55 +0900

    feat(Museum): add Stack

--- src/components/Museum.tsx
+++ src/components/Museum.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Chip, Stack, Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { getMuseum } from '../data';
 
@@ -14,7 +14,9 @@ export const Museum = () => {
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
           {museum.access}
         </Typography>
-
+        <Stack direction="row" spacing={1}>
+          <Chip label={museum.address} size="small" />
+        </Stack>
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
