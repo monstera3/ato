bfb7aeb70e2962a47970ae4e894b9dfb66bc8ba2
natsumi <aka.723n@gmail.com>
2022-05-09 13:25:39 +0900

    feat: add icon, secondary

--- src/components/Museums.tsx
+++ src/components/Museums.tsx
@@ -1,7 +1,12 @@
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import MuseumIcon from '@mui/icons-material/Museum'
+
 export const Museums=()=> {
   return(
-    <div>
-      MuseumList
-    </div>
+    <List>
+      <ListItem><ListItemIcon><MuseumIcon /></ListItemIcon><ListItemText primary="Single-line item" secondary="secondary"/></ListItem>
+      <ListItem><ListItemIcon><MuseumIcon /></ListItemIcon><ListItemText primary="Single-line item" secondary="secondary"/></ListItem>
+      <ListItem><ListItemIcon><MuseumIcon /></ListItemIcon><ListItemText primary="Single-line item" secondary="secondary"/></ListItem>
+    </List>
   );
 };
