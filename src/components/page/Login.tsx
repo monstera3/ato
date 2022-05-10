8f5a65fa809a58ee4d1468a6d2bf452783b373f0
natsumi <aka.723n@gmail.com>
2022-05-10 15:28:36 +0900

    feat: add google login

--- src/components/page/Login.tsx
+++ src/components/page/Login.tsx
@@ -1,7 +1,20 @@
+import { useAuth } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+
+
 export const Login=()=> {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = () => {
+    auth.googleLogin(() => {
+      navigate("/", { replace: true })
+    });
+  }
   return(
     <div>
-      Login
+      <Button onClick={handleGoogleLogin}>Googleログイン</Button>
     </div>
   );
 };
