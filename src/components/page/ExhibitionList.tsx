83223787b23921f7a1c7805e2f12ea79282b5199
natsumi <aka.723n@gmail.com>
2022-05-12 14:05:35 +0900

    feat: ExhibitionList.tsx add Exhibitioncard

--- src/components/page/ExhibitionList.tsx
+++ src/components/page/ExhibitionList.tsx
@@ -1,7 +1,25 @@
+import { Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { ExhibitionCard } from '../model/ExhibitionCard';
+import { getMuseums } from '../../data';
+
 export const ExhibitionList=()=> {
+
+  const museums = getMuseums();
+
   return(
     <div>
       ExhibitionList
+      <Grid container spacing={2}>
+        {museums.map((museum)=>(
+          <Grid item xs={12} sm={6} md={4} lg={3} key={museum.id}>
+            <Link to={'/museums/'+museum.id}>
+              <ExhibitionCard />
+            </Link>
+
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
