458b878da739a7928fa53ad982f740f3b76cf943
natsumi <aka.723n@gmail.com>
2022-05-12 12:52:55 +0900

    style: button 真ん中に修正

--- src/components/page/FeaturedExhibition.tsx
+++ src/components/page/FeaturedExhibition.tsx
@@ -15,20 +15,21 @@ export const FeaturedExhibition=()=> {
         <Typography gutterBottom variant="h5" component="div">
           注目の展覧会
         </Typography>
-        <Container
-          sx={{ my: 5,backgroundColor: '#f5f5f5',display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'}}>
-        <ExhibitionCard />
-        <ExhibitionCard />
-        <ExhibitionCard />
+        <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Container
+            sx={{ my: 5,backgroundColor: '#f5f5f5',display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between'}}>
+            <ExhibitionCard />
+            <ExhibitionCard />
+            <ExhibitionCard />
+          </Container>
+          <Button
+            sx={{ my: 5, backgroundColor: '#f5f5f5', width: 100 }}
+            variant="outlined" endIcon={<ArrowRightAltIcon />}>
+            more
+          </Button>
         </Container>
-        <Button
-          sx={{ my: 5,backgroundColor: '#f5f5f5'
-            }}
-          variant="outlined" endIcon={<ArrowRightAltIcon />}>
-          more
-        </Button>
       </Container>
 
     </div>
