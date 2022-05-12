7776f2ac6705da8b1268b035506d2104b041c44c
natsumi <aka.723n@gmail.com>
2022-05-12 15:25:23 +0900

    feat: add loginボタン

--- src/components/page/Login.tsx
+++ src/components/page/Login.tsx
@@ -1,12 +1,31 @@
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  Dialog,
+  DialogTitle, DialogContent, DialogContentText, DialogActions
+} from '@mui/material';
 import React from 'react';
 import ClearIcon from '@mui/icons-material/Clear';
 import GoogleIcon from '@mui/icons-material/Google';
 
 
 export const Login=()=> {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const auth = useAuth();
   const navigate = useNavigate();
 
@@ -40,9 +59,45 @@ export const Login=()=> {
         <CardActions >
           <Button onClick={handleGoogleLogin}
                   sx={{ m:'auto',backgroundColor: '#f5f5f5'}}
-                  variant="outlined" startIcon={<GoogleIcon />}>Login with Google</Button>
+                  variant="outlined" startIcon={<GoogleIcon />}>
+            Login with Google</Button>
         </CardActions>
       </Card>
+      <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          loginボタン
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title" sx={{ textAlign:'center'}}>
+            {"Ato LOGO"}
+          </DialogTitle>
+          <DialogContent sx={{ textAlign:'center'}}>
+            <DialogContentText id="alert-dialog-description">
+              Artはアート情報共有プラットフォームです。
+              <br />
+              展示や美術館の感想をシェアしましょう。
+            </DialogContentText>
+            <DialogContentText
+              sx={{ my: 1.5 }} variant="body2" color="text.secondary"
+              id="alert-dialog-description">
+              利用規約、プライバシーポリシーに同意したうえでログインしてください。
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleGoogleLogin}
+                    sx={{ m:'auto',backgroundColor: '#f5f5f5'}}
+                    variant="outlined" startIcon={<GoogleIcon />}>
+              Login with Google</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+
+
     </div>
   );
 }
