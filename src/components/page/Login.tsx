67108df69bdbdc083a739bad4b57a7f61e789621
natsumi <aka.723n@gmail.com>
2022-05-13 12:13:55 +0900

    fix: 不要なコードを削除

--- src/components/page/Login.tsx
+++ src/components/page/Login.tsx
@@ -2,9 +2,6 @@ import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import {
   Button,
-  Card,
-  CardActions,
-  CardContent,
   Typography,
   Dialog,
   DialogTitle, DialogContent, DialogContentText, DialogActions
@@ -36,33 +33,6 @@ export const Login=()=> {
   }
   return(
     <div>
-      <Card sx={{ minWidth: 275 ,maxWidth:500,m:'auto',backgroundColor:'#F0EFE0'}}>
-        <Typography
-          sx={{ fontSize: 14 ,textAlign:'right',mr:3,mt:3}} color="text.secondary" gutterBottom>
-          <a href="http://localhost:3001/login"><ClearIcon /></a>️
-        </Typography>
-        <CardContent sx={{ textAlign:'center'}}>
-
-          <Typography variant="h5" component="div">
-            Ato LOGO
-          </Typography>
-
-          <Typography sx={{ my: 1.5}} variant="body1">
-            Artはアート情報共有プラットフォームです。
-            <br />
-            展示や美術館の感想をシェアしましょう。
-          </Typography>
-          <Typography sx={{ my: 1.5 }} variant="body2" color="text.secondary">
-            利用規約、プライバシーポリシーに同意したうえでログインしてください。
-          </Typography>
-        </CardContent>
-        <CardActions >
-          <Button onClick={handleGoogleLogin}
-                  sx={{ m:'auto',backgroundColor: '#f5f5f5'}}
-                  variant="outlined" startIcon={<GoogleIcon />}>
-            Login with Google</Button>
-        </CardActions>
-      </Card>
       {/*TODO: 右上のログインをクリックしたらDialogが出てくるようにしたい*/}
       <div>
         <Button variant="outlined" onClick={handleClickOpen}>
