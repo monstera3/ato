67a28974c3d21b594f0bf8b947ea07e7af17d756
natsumi <aka.723n@gmail.com>
2022-05-14 12:20:20 +0900

    refactor: extract UserReviews

--- src/components/page/UserDetail.tsx
+++ src/components/page/UserDetail.tsx
@@ -8,7 +8,7 @@ import {
   Typography
 } from '@mui/material';
 import { useParams } from 'react-router-dom';
-import { getUser } from '../../data';
+import { getUser, UserType } from '../../data';
 import sampleImg from '../../assets/images/sample1.png'
 import { Review } from '../model/Review';
 
@@ -37,28 +37,32 @@ export const UserDetail = () => {
       </Container>
 
       {/*TODO: タブを追加したい*/}
-      <Card sx={{ minWidth: 275 }}>
-        a
-        <CardContent>
-          <Typography variant="h5" component="div">
-            {user.displayName}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            {user.id}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            {user.comment}
-          </Typography>
-          <Stack direction="row" spacing={1}>
-            {
-              user.watchedMuseum.map((watchedMuseum) => (
-                <Review user={user} museumId={watchedMuseum.museumId} key={watchedMuseum.museumId} />
-              ))
-            }
-          </Stack>
-        </CardContent>
-      </Card>
+      <UserReviews user={user} />
     </>
-
+  )
+}
+const UserReviews = (props: { user: UserType }) => {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      a
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {props.user.displayName}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {props.user.id}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {props.user.comment}
+        </Typography>
+        <Stack direction="row" spacing={1}>
+          {
+            props.user.watchedMuseum.map((watchedMuseum) => (
+              <Review user={props.user} museumId={watchedMuseum.museumId} key={watchedMuseum.museumId} />
+            ))
+          }
+        </Stack>
+      </CardContent>
+    </Card>
   )
 }
