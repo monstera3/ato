0c048b64cec4629f1193093118ab8d85283fa73f
sughimura <nxsughi@gmail.com>
2022-05-15 12:39:55 +0900

    feat(app): add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,35 @@ import { UserDetail } from './components/page/UserDetail';
 import { Toolbar } from '@mui/material';
 import { AuthProvider } from './context/AuthContext'
 import { ExhibitionDetail } from './components/page/ExhibitionDetail';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export const App=()=> {
   return (
-    <AuthProvider>
-      <div className="App">
-      <ResponsiveAppBar/>
-      <SearchBar/>
-      <Toolbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="about" element={<About />}/>
-        <Route path="museums">
-          <Route index element={<MuseumList />} />
-          <Route path=":museumId" element={<MuseumDetail />}/>
-        </Route>
-        <Route path="users">
-          <Route path=":userId" element={<UserDetail />}/>
-        </Route>
-        <Route path="exhibitions">
-          <Route index element={<ExhibitionList />} />
-          <Route path=":exhibitionId" element={<ExhibitionDetail />}/>
-        </Route>
-      </Routes>
-      </div>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <div className="App">
+          <ResponsiveAppBar/>
+          <SearchBar/>
+          <Toolbar />
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="about" element={<About />}/>
+            <Route path="museums">
+              <Route index element={<MuseumList />} />
+              <Route path=":museumId" element={<MuseumDetail />}/>
+            </Route>
+            <Route path="users">
+              <Route path=":userId" element={<UserDetail />}/>
+            </Route>
+            <Route path="exhibitions">
+              <Route index element={<ExhibitionList />} />
+              <Route path=":exhibitionId" element={<ExhibitionDetail />}/>
+            </Route>
+          </Routes>
+        </div>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
