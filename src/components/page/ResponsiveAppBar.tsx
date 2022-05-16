7cf4991d7764eb7b720e779c32446ae61a848b77
natsumi <aka.723n@gmail.com>
2022-05-16 14:23:02 +0900

    fix(error): 'GET http://localhost:3002/static/images/avatar/2.jpg 404… … (Not Found)'

--- src/components/page/ResponsiveAppBar.tsx
+++ src/components/page/ResponsiveAppBar.tsx
@@ -14,6 +14,7 @@ import MenuItem from '@mui/material/MenuItem';
 import { Link } from '@mui/material';
 import { Login } from './Login';
 import LogoutIcon from '@mui/icons-material/Logout';
+import sampleImg from '../../assets/images/sample1.png';
 
 
 type PageType = {
@@ -128,7 +129,7 @@ const ResponsiveAppBar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src={sampleImg} />
               </IconButton>
             </Tooltip>
             <Menu
