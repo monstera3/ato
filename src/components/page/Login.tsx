78e41dea99cfe01ba3dc81d46e5779a94f4b6d80
natsumi <aka.723n@gmail.com>
2022-05-16 10:31:46 +0900

    refactor: 不要なdivタグを削除

--- src/components/page/Login.tsx
+++ src/components/page/Login.tsx
@@ -33,45 +33,44 @@ export const Login=()=> {
   }
   return(
     <div>
-      {/*TODO: 右上のログインをクリックしたらDialogが出てくるようにしたい*/}
       <div>
         <div onClick={handleClickOpen}>
           Login
         </div>
-        <Dialog
-          open={loginDialogOpen}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <Typography
-            sx={{ fontSize: 14 ,textAlign:'right',mr:1,mt:2}} color="text.secondary" gutterBottom>
-            <Button onClick={handleClose}><ClearIcon /></Button>️
-          </Typography>
-          <DialogTitle id="alert-dialog-title" sx={{ textAlign:'center'}}>
-            {"Ato LOGO"}
-          </DialogTitle>
-          <DialogContent sx={{ textAlign:'center'}}>
-            <DialogContentText id="alert-dialog-description">
-              Artはアート情報共有プラットフォームです。
-              <br />
-              展示や美術館の感想をシェアしましょう。
-            </DialogContentText>
-            <DialogContentText
-              sx={{ my: 1.5 }} variant="body2" color="text.secondary"
-              id="alert-dialog-description">
-              利用規約、プライバシーポリシーに同意したうえでログインしてください。
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleGoogleLogin}
-                    sx={{ m:'auto',backgroundColor: '#f5f5f5'}}
-                    variant="outlined" startIcon={<GoogleIcon />}>
-              Login with Google</Button>
-          </DialogActions>
-        </Dialog>
-      </div>
 
+      </div>
+      <Dialog
+        open={loginDialogOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <Typography
+          sx={{ fontSize: 14 ,textAlign:'right',mr:1,mt:2}} color="text.secondary" gutterBottom>
+          <Button onClick={handleClose}><ClearIcon /></Button>️
+        </Typography>
+        <DialogTitle id="alert-dialog-title" sx={{ textAlign:'center'}}>
+          {"Ato LOGO"}
+        </DialogTitle>
+        <DialogContent sx={{ textAlign:'center'}}>
+          <DialogContentText id="alert-dialog-description">
+            Artはアート情報共有プラットフォームです。
+            <br />
+            展示や美術館の感想をシェアしましょう。
+          </DialogContentText>
+          <DialogContentText
+            sx={{ my: 1.5 }} variant="body2" color="text.secondary"
+            id="alert-dialog-description">
+            利用規約、プライバシーポリシーに同意したうえでログインしてください。
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleGoogleLogin}
+                  sx={{ m:'auto',backgroundColor: '#f5f5f5'}}
+                  variant="outlined" startIcon={<GoogleIcon />}>
+            Login with Google</Button>
+        </DialogActions>
+      </Dialog>
 
     </div>
   );
